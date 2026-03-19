126172cda781be1eec076cc9a265fd09109be4d7
James Publico <bienvenido.publico@cdsp.edu.ph>
2026-03-19 12:56:51 +0800

    Update

--- database/migrations/2026_03_17_125424_add_suspended_at_to_users.php
+++ database/migrations/2026_03_17_125424_add_suspended_at_to_users.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->timestamp('suspended_at')->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('suspended_at');
         });
     }
 };
